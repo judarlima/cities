30c75d865d262d31bdabd030b52b90d1a56e8a20
Judar Lima <judar.rslima@gmail.com>
2019-02-06 17:05:05 -0200

    Implements City Cell View.

--- Cities/Scenes/CitiesList/Views/CityTableViewCell.swift
+++ Cities/Scenes/CitiesList/Views/CityTableViewCell.swift
@@ -9,19 +9,29 @@
 import UIKit
 
 class CityTableViewCell: UITableViewCell {
-    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet private weak var nameLabel: UILabel!
+    @IBOutlet private weak var cellContentView: UIView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
+        setupView()
     }
     
     func bind(viewModel: CityViewModel) {
         nameLabel.text = viewModel.name
     }
-
+    
+    private func setupView() {
+        backgroundColor = .clear
+        cellContentView.layer.borderColor = UIColor.black.cgColor
+        cellContentView.layer.borderWidth = 1
+        cellContentView.layer.cornerRadius = 8
+        cellContentView.clipsToBounds = true
+    }
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-        super.setSelected(!selected, animated: animated)
+        super.setSelected(false, animated: false)
     }
     
 }
