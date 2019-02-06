a27402bacd1bbf0db6fb9f800dfb588f777dbf17
Judar Lima <judar.rslima@gmail.com>
2019-02-06 13:29:53 -0200

    Instantiate the coordinator to handle with app flow.

---
+++ Cities/Scenes/CitiesList/View/CitiesListViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CitiesListViewController.swift
+//  Cities
+//
+//  Created by Judar Lima on 06/02/19.
+//  Copyright © 2019 raduJ. All rights reserved.
+//
+
+import UIKit
+
+class CitiesListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
