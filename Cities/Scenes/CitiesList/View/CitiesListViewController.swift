c03d62c836caf335ab1755a59120902ee1e44091
Judar Lima <judar.rslima@gmail.com>
2019-02-06 13:33:29 -0200

    Creates CitiesListViewController.

--- Cities/Scenes/CitiesList/View/CitiesListViewController.swift
+++ Cities/Scenes/CitiesList/View/CitiesListViewController.swift
@@ -9,22 +9,35 @@
 import UIKit
 
 class CitiesListViewController: UIViewController {
-
+    private weak var presenter: CitiesListPresenterLogic?
+    private weak var interactor: CitiesListInteractorLogic?
+    private var viewModel = [CityViewModel]()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    init(interactor: CitiesListInteractorLogic,
+         presenter: CitiesListPresenterLogic) {
+        super.init(nibName: "CitiesListViewController", bundle: Bundle.main)
+        self.presenter = presenter
+        self.interactor = interactor
     }
-    */
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
 
+extension CitiesListViewController: CitiesListDisplayLogic {
+    func displayError(message: String) {
+        //
+    }
+    
+    func displayCities(viewModel: [CityViewModel]) {
+        self.viewModel = viewModel
+    }
 }
