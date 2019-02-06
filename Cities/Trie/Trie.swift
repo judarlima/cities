cd5b6154f3c116970d37da3e7e3152cf5f4585fc
Judar Lima <judar.rslima@gmail.com>
2019-02-06 13:34:09 -0200

    Creates the prefix tree to improve the performance on searchs.

--- Cities/Trie/Trie.swift
+++ Cities/Trie/Trie.swift
@@ -7,3 +7,86 @@
 //
 
 import Foundation
+
+class Trie<T> {
+    typealias Node = TrieNode<Character, T>
+    
+    fileprivate let root: Node
+    fileprivate var wordCount: Int
+    
+    init() {
+        root = Node()
+        wordCount = 0
+    }
+    
+    public var words: [T] {
+        return wordsInSubtrie(rootNode: root, partialWord: "")
+    }
+}
+
+extension Trie {
+    func insert(word: String, data: T) {
+        guard !word.isEmpty else { return }
+        
+        var currentNode = root
+        for character in word.lowercased() {
+            if let childNode = currentNode.children[character] {
+                currentNode = childNode
+            } else {
+                currentNode.add(value: character)
+                currentNode = currentNode.children[character]!
+            }
+        }
+        
+        wordCount += 1
+        currentNode.isTerminating = true
+        currentNode.data = data
+    }
+    
+    private func findLastNodeOf(word: String) -> Node? {
+        var currentNode = root
+        for character in word.lowercased() {
+            guard let childNode = currentNode.children[character] else {
+                return nil
+            }
+            currentNode = childNode
+        }
+        return currentNode
+    }
+    
+    fileprivate func wordsInSubtrie(rootNode: Node, partialWord: String) -> [T] {
+        var subtrieWords = [T]()
+        var previousLetters = partialWord
+        
+        if let value = rootNode.value {
+            previousLetters.append(value)
+        }
+        
+        if rootNode.isTerminating {
+            subtrieWords.append(rootNode.data!)
+        }
+        
+        for childNode in rootNode.children.values {
+            let childWords = wordsInSubtrie(rootNode: childNode, partialWord: previousLetters)
+            subtrieWords += childWords
+        }
+        
+        return subtrieWords
+    }
+    
+    func findWordsWithPrefix(prefix: String) -> [T] {
+        var words = [T]()
+        let prefixLowerCased = prefix.lowercased()
+        if let lastNode = findLastNodeOf(word: prefixLowerCased) {
+            if lastNode.isTerminating {
+                words.append(lastNode.data!)
+            }
+            
+            for childNode in lastNode.children.values {
+                let childWords = wordsInSubtrie(rootNode: childNode, partialWord: prefixLowerCased)
+                words += childWords
+            }
+        }
+        return words
+    }
+}
